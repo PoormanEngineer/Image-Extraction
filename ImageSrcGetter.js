bd807d549b92454b08a4973e5f9296771583a68b
PoormanEngineer <59951877+PoormanEngineer@users.noreply.github.com>
2020-04-07 07:03:39 +0900

    use sync-request

--- ImageSrcGetter.js
+++ ImageSrcGetter.js
@@ -1,31 +1,31 @@
-const request = require('request');
+const request = require('sync-request');
 const cheerio = require('cheerio');
 
-let options = {
-    url: '',
-    method: 'GET'
-}
-
 module.exports = {
-    get: function(targetUrl) {
-        options.url = targetUrl;
-        
-        console.log('sendRequest : ' + options.url);
+    get: function(params) {
+        let targetUrl   = params.url;
+        let minWidth    = params.minWidth;
+        let minHeight   = params.minHeight;
+        let containsGif = params.containsGif;
 
-        let imgUrls = null;
-        
-        request(options, function(error, response, body){
-            const $ = cheerio.load(body);
+        let res = request('GET', targetUrl);
 
-            imgUrls = $('img').get().map(img => {
-                return img.attribs.src.split('?')[0];
-            })
-            .filter(url => !url.endsWith('.gif'));
+        let $ = cheerio.load(res.getBody());
 
-            console.log(imgUrls);
-
-            imgUrls.forEach(function(url) {
-            });
+        let imgUrls = $('img').get().map(img => {
+            return img.attribs.src.split('?')[0];
         });
+
+        if(!containsGif) {
+            imgUrls = imgUrls.filter(img => !img.endsWith('.gif'));
+        }
+
+        let imgTags = '';
+        
+        imgUrls.forEach(function(url) {
+            imgTags += '<img src="url" />'.replace('url', url);
+        });
+
+        return imgTags;
     }
 }
--- server.js
+++ server.js
@@ -1,36 +1,37 @@
-let http = require('http');
-let url = require('url');
-let fs = require('fs');
-let imageSrcGetter = require('./ImageSrcGetter.js');
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const ejs = require('ejs');
+const imageSrcGetter = require('./ImageSrcGetter.js');
 
-let server = http.createServer();
+const imageExtractionEjs = fs.readFileSync('./Image-Extraction.ejs', 'utf-8');
+
+const server = http.createServer();
 
 server.on('request', function(req, res) {
-    let url_parse = url.parse(req.url, true);
-    let targetUrl = url_parse.query.url;
+    let url_parse   = url.parse(req.url, true);
+    let params      = url_parse.query;
 
-    console.log(url_parse);
-    console.log(targetUrl);
+    // console.log(url_parse);
+    console.log(params);
 
     if(url_parse.pathname === '/Image-Extraction') {
-        if(targetUrl == null) {
-            fs.readFile('./Image-Extraction.html', 'utf-8', function(err, data) {
-                if(err) {
-                    res.writeHead(404, {'Content-Type': 'text/plain'});
-                    res.write('Error');
-                    res.end();
-                    return;
-                }
+        let msg = '';
+        let imgTags = '';
 
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                res.end();
-            });
+        if(params.url != null) {
+            imgTags = imageSrcGetter.get(params);
+
+            if(imgTags === '') {
+                msg = '画像が取得できませんでした';
+            }
         }
-        else {
-            imageSrcGetter.get(targetUrl);
-            res.end();
-        }
+
+        let resultHtml = ejs.render(imageExtractionEjs, {imgTags: imgTags, msg: msg});
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(resultHtml);
+        res.end();
     }
     else {
         res.writeHead(404, {'Content-Type': 'text/plain'});
